8dbe7a619470d686ca33318577b10a487aba9749
Jens-Rene Giesen <jensrene.giesen@gmail.com>
2017-01-07 18:13:51 +0100

    Accounting angepasst

--- ipark/AccountingBillingService.py
+++ ipark/AccountingBillingService.py
@@ -2,32 +2,22 @@ from communication.Service import Service
 from communication.Client import Client
 import AuthService
 import redis
+from model.User import User
 
 
 class AccountingAndBillingService(Service):
     def __init__(self):
         self.authservice = AuthService.AuthClient()
-        self.r = redis.StrictRedis(host='132.252.152.57')
-        self.r.execute_command("AUTH GS~FsB3~&c7T")
         super().__init__("Accounting")
 
     def sign_up(self, email, password):
-        key = "user:"+email
-        key = key.encode()
-        if self.r.exists(key):
+        if User.create(email) is not None:
             return {"status": False, "message": "Mail address is already in use."}
-        u = {b"email": email.encode(), b"password": password.encode()}  # Todo weitere Userdaten
-        self.r.hmset(key, u)
         result = self.authservice.login(email, password)
         return {"status": True, "token": result["token"]}
 
     def validate_user(self, email, password):
-        key = "user:" + email
-        key = key.encode()
-        if not self.r.exists(key):
-            return {"status": False, "message": "Invalid mail address or password."}
-        user = self.r.hgetall(key)
-        if password.encode() != user[b"password"]:
+        if User(email, password) is None:
             return {"status": False, "message": "Invalid mail address or password."}
         return {"status": True}
 
