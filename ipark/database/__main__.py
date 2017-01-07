ab68e9ce2b0b97c6b1b7a1a2cb277c1b9ecf299f
Tcc100 <Tob@wolke7.net>
2017-01-07 03:53:52 +0100

    Redis/MySQL Example for geospatial Data v2

--- ipark/database/__main__.py
+++ ipark/database/__main__.py
@@ -1,11 +1,13 @@
 import redis
 import MySQLdb as mydb
 import MySQLdb.cursors
+import pickle
 
 r = redis.StrictRedis(host='132.252.152.57')
 r.execute_command("AUTH GS~FsB3~&c7T")
 
 # r.flushall()
+# r.delete('parkinglots')
 
 # fill redis if not exist
 if not r.exists('parkinglots'):
@@ -13,21 +15,19 @@ if not r.exists('parkinglots'):
                       cursorclass=mydb.cursors.DictCursor) as cur:  # type: MySQLdb.cursors.DictCursor
 
         cur.execute('SELECT * FROM parking_lots NATURAL LEFT JOIN parking_lot_status')
-
         rows = cur.fetchall()
+
         geoadd_command = ['GEOADD', 'parkinglots']
-        for row in rows:  # type:dict
-            geoadd_command.extend([str(row['longitude']), str(row['latitude']), str(row['lot_id'])])
-            r.hmset('lot:' + str(row['lot_id']), row)
+        for row in rows:  # type: dict
+            geoadd_command.extend([str(row['longitude']), str(row['latitude']), pickle.dumps(row)])
         r.execute_command(*geoadd_command)
 
 
 def find_near_parking_lots(rds: redis.StrictRedis, lon: float, lat: float, radius: int) -> list:
-    resp = rds.execute_command('GEORADIUS', 'parkinglots', lon, lat, radius, 'km')  # type: list[bytes]
-    lots = [rds.hgetall('lot:' + b.decode()) for b in resp]
-    return lots
+    lots = r.execute_command('GEORADIUS', 'parkinglots', str(lon), str(lat), str(radius), 'km')
+    return [pickle.loads(l) for l in lots]
 
 
-lots = find_near_parking_lots(r, 51.4, 7, 6)
+lots = find_near_parking_lots(r, 51.4, 7.03, 6)
 print(lots)
 print(len(lots))
