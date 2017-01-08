2742ec013e38048deb8701fa75b5804ccb8d8401
Jens-Rene Giesen <jensrene.giesen@gmail.com>
2017-01-08 20:48:31 +0100

    Danke Tobi xD

--- ipark/communication/Client.py
+++ ipark/communication/Client.py
@@ -9,8 +9,8 @@ import pika
 class Client:
     def __init__(self, name=None):
         self.name = name or self.__class__.__name__
-        self.response = None
-        self.corr_id = None
+        self.responses = {}
+        self.corr_ids = []
 
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host='132.252.152.56',
@@ -23,28 +23,27 @@ class Client:
         self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
 
     def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = pickle.loads(body)
-            self.corr_id = None
+        if props.correlation_id in self.corr_ids:
+            self.responses[props.correlation_id] = pickle.loads(body)
+            self.corr_ids.remove(props.correlation_id)
 
     def call(self, function, *args, **kwargs):
-        if self.corr_id is not None:
-            raise 0  # Todo
-
         request = pickle.dumps({'function': function, 'args': args, 'kwargs': kwargs}, pickle.HIGHEST_PROTOCOL)
 
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
+        corr_id = str(uuid.uuid4())
+        self.corr_ids.append(corr_id)
         self.channel.basic_publish(exchange='',
                                    routing_key=self.name,
                                    properties=pika.BasicProperties(reply_to=self.callback_queue,
-                                                                   correlation_id=self.corr_id, ),
+                                                                   correlation_id=corr_id, ),
                                    body=request)
-        while self.response is None:
+        while corr_id not in self.responses:
             self.connection.process_data_events()
             self.connection.sleep(0.01)
 
-        if 'exception' in self.response:
-            raise self.response['exception']
+        response = self.responses.pop(corr_id)
 
-        return self.response['result']
+        if 'exception' in response:
+            raise response['exception']
+
+        return response['result']
