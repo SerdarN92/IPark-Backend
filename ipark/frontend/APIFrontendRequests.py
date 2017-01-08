364410bd53e0a94e89d09902776c9a93e8d32e92
Jens-Rene Giesen <jensrene.giesen@gmail.com>
2017-01-08 18:01:43 +0100

    sign_up, login are ready. user_status_get started.
    They now use our services :-P

--- ipark/frontend/APIFrontendRequests.py
+++ ipark/frontend/APIFrontendRequests.py
@@ -1,33 +1,34 @@
-from flask_restplus import Api, marshal
+from flask_restplus import Api
+from AuthService import AuthClient
+from AccountingBillingService import AccountingAndBillingClient
 from flask import request
 from hashlib import md5
 
 users = {}  # type: dict[str: User]
+auth_client = AuthClient()
+accounting_client = AccountingAndBillingClient()
 
 
 def user_signup(api: Api):
-    m = generateToken(api.payload['email'], api.payload['password'])
-    if m not in users:
-        users[m] = api.payload
-        return user_login(api)
+    sign_up_result = accounting_client.sign_up(api.payload['email'], api.payload['password'])
+    if "token" in sign_up_result:
+        return {"token": sign_up_result["token"]}, 200
     else:
         api.abort(422, "User already exists")
 
 
 def user_login(api: Api):
-    m = generateToken(api.payload['email'], api.payload['password'])
-    if m in users:
-        return {'token': m}, 200
+    login_result = auth_client.login(api.payload["email"], api.payload["password"])
+    if "token" in login_result:
+        return {'token': login_result["token"]}, 200
     api.abort(422, "Invalid Credentials")
 
 
 def user_status_get(api: Api, **kwargs):
-    if 'X-Token' not in request.headers or request.headers['X-Token'] not in users:
+    if 'X-Token' not in request.headers or not auth_client.validate_token(request.headers["X-Token"])["status"]:
         api.abort(401, "Invalid Token")
         return
-    user = users[request.headers['X-Token']]
-
-    return {'info': {'lastname': user.get('lastname', 'noname')}}, 200
+    return {'info': {'lastname': 'noname'}}, 200
 
 
 def user_status_set(api: Api, **kwargs):
