5a6b66f1d8878b775b6c837b83c4eac04f251905
Jens-Rene Giesen <jensrene.giesen@gmail.com>
2017-01-31 22:11:59 +0100

    Ortswechsel

--- ipark/PollingService.py
+++ ipark/PollingService.py
@@ -1,8 +1,11 @@
 from communication.Service import Service
 from communication.Client import Client
 from model.ParkingLot import ParkingLot
+from model.DomainClasses import Reservation
+from AccountingBillingService import AccountingAndBillingService
 import requests
 from flask import json
+from model.User import User
 
 
 class PollingService(Service):
@@ -13,13 +16,21 @@ class PollingService(Service):
     def poll_lot(self, lot_id):
         lot = ParkingLot(lot_id)
         full_url = lot.api_path + "/events"
-        response = requests.get(full_url)
-        json_to_parse = response.content.decode().split('\r\n\r\n', 1)[1]
-        events = json.loads(json_to_parse)
+        response = requests.get(full_url)  # todo certificate
+        if response.status_code != 200:
+            return
+        events = response.json()
         # [{"index":2,"startTime":77774128,"stopTime":77774375,"ID":57,"spotid":16843009}]
         for event in events:
-            # todo fetch reservation
-            # todo fetch user
+            res_id = event["ID"]
+            email = Reservation.get_email_from_resid(res_id)
+            if not email:
+                continue
+            user = User(email, readonly=True)
+            reservation = AccountingAndBillingService.get_user_reservation_for_id(user, id)
+            if not reservation:
+                continue
+
             # todo check if the reservation is running
             pass
 
